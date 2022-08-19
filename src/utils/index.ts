8d308c1a4ba986de6fc416c081bed4ee299302dc
Sasha_work <kalinovsasha@yandex.ru>
2022-08-19 10:27:52 +0300

    feat: add func shuffle

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,3 +2,11 @@ export function throwConsoleError(message: string, error: unknown) {
   const errMessage = (error instanceof Error) ? error.message : 'Unknown Error';
   console.error(`${message} ${errMessage}`);
 }
+
+// Функция перемешивания массивов https://en.wikipedia.org/wiki/Fisher–Yates_shuffle
+export function shuffle<T>(arr: Array<T>) {
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+}

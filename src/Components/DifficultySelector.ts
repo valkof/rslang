eddc0f6d2f6a1261030d2a6e5d1f9683e08f723a
Kalinov Alexander <kalinovsasha@yandex.ru>
2022-08-20 16:08:06 +0300

    refactor: change dificulty styles

--- src/Components/DifficultySelector.ts
+++ src/Components/DifficultySelector.ts
@@ -2,18 +2,21 @@ import { Component } from '../Abstract/component';
 
 export class DifficultySelector extends Component {
   constructor(parent: HTMLElement, style: string, title: string, text: string) {
-    super(parent, 'div', [`${style}-dificulty`]);
-    const dificultyTitle = new Component(this.root, 'h2', [`${style}-dificulty__title`]);
+    super(parent, 'div', [`${style}-dificulty`, 'dificulty']);
+    const dificultyTitle = new Component(this.root, 'h2', [`${style}-dificulty__title`, 'dificulty__title']);
     dificultyTitle.root.textContent = title;
-    const dificultyDescription = new Component(this.root, 'h3', [`${style}-dificulty__description`]);
+    const dificultyDescription = new Component(this.root, 'h3', [
+      `${style}-dificulty__description`,
+      'dificulty__description',
+    ]);
     dificultyDescription.root.textContent = text;
-    const levelContainer = new Component(this.root, 'div', [`${style}-dificulty__level-container`]);
+    const levelContainer = new Component(this.root, 'div', [`dificulty__level-container`]);
     for (let i = 0; i < 6; i++) {
       const item = new Component(levelContainer.root, 'div', [
-        `${style}-dificulty__level-container__item`,
-        `dificulty__level-container__item${i+1}`,
+        `dificulty__level-container__item`,
+        `dificulty__level-container__item${i + 1}`,
       ]);
-      item.root.textContent = (i+1).toString();
+      item.root.textContent = (i + 1).toString();
     }
   }
 }

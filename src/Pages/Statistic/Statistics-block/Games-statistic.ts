cd7cf5e1f3688b68fa995b9028aa8c4f51ab3ef0
Victoria Belova <vacova1988@gmail.com>
2022-09-03 14:00:36 +0300

    feat: add a table and delete two buttons in Statistic page

--- src/Pages/Statistic/Statistics-block/Games-statistic.ts
+++ src/Pages/Statistic/Statistics-block/Games-statistic.ts
@@ -2,62 +2,29 @@ import { Component } from '../../../Abstract/component';
 
 export class GamesStatistic extends Component {
  
-  selectGame: Component;
- 
-  selectBtnAudioCall: Component;
- 
-  selectBtnSprint: Component;
- 
-  gameStatisticTitle: Component;
- 
-  statistiFromGames: Component;
- 
-  learnedWords: Component;
- 
-  countWords: Component;
- 
-  countTitle: Component;
- 
-  numberPercent: Component;
- 
-  rightAnswers: Component;
- 
-  percetnrigrhtAnswers: Component;
- 
-  rightAnswersTitle: Component;
- 
-  rightAnswersPercent: Component;
- 
-  bestGame: Component;
- 
-  bestGameBlock: Component;
- 
-  bestGameTitle: Component;
- 
-  bestGameCount: Component;
- 
-  constructor(parent: HTMLElement) {
+    constructor(parent: HTMLElement) {
     super(parent, 'div', ['games-statistic-wrapper']);
-    this.gameStatisticTitle = new Component(this.root, 'h3', [], 'Статистика по мини-играм');
+    ['Статистика Спринт', 'Статистика Аудиовызов'].forEach(el => {
+      let gameStatisticTitle = new Component(this.root, 'h3', [], el);
      
-    this.statistiFromGames = new Component(this.root, 'div', ['statistic-games']);
-    this.learnedWords = new Component(this.statistiFromGames.root, 'div', ['learned-words']);
-    this.countWords = new Component(this.learnedWords.root, 'div', ['count-words']);
-    this.countTitle = new Component(this.countWords.root, 'p', [], 'Количество новых слов');
-    this.numberPercent = new Component(this.countWords.root, 'p', [], '');
+      let statistiFromGames = new Component(this.root, 'div', ['statistic-games']);
+      let learnedWords = new Component(statistiFromGames.root, 'div', ['learned-words']);
+      let countWords = new Component(learnedWords.root, 'div', ['count-words']);
+      let countTitle = new Component(countWords.root, 'p', [], 'Количество новых слов');
+      let numberPercent = new Component(countWords.root, 'p', [], '');
+  
+      let rightAnswers = new Component(statistiFromGames.root, 'div', ['right-answers']);
+      let percetnrigrhtAnswers = new Component(rightAnswers.root, 'div', ['percent-right-ancwers']);
+      let rightAnswersTitle = new Component(percetnrigrhtAnswers.root, 'p', [], 'Процент правильных ответов');
+      let rightAnswersPercent = new Component(percetnrigrhtAnswers.root, 'p', [], '%');
+  
+      let bestGameBlock = new Component(statistiFromGames.root, 'div', ['best-game-block']);
+      let bestGame = new Component(bestGameBlock.root, 'div', ['best-game']);
+      let bestGameTitle = new Component(bestGame.root, 'p', [], 'Лучшая игра');
+      let bestGameCount = new Component(bestGame.root, 'p', [], '');
+    })
+    
 
-    this.rightAnswers = new Component(this.statistiFromGames.root, 'div', ['right-answers']);
-    this.percetnrigrhtAnswers = new Component(this.rightAnswers.root, 'div', ['percent-right-ancwers']);
-    this.rightAnswersTitle = new Component(this.percetnrigrhtAnswers.root, 'p', [], 'Процент правильных ответов');
-    this.rightAnswersPercent = new Component(this.percetnrigrhtAnswers.root, 'p', [], '%');
-
-    this.bestGameBlock = new Component(this.statistiFromGames.root, 'div', ['best-game-block']);
-    this.bestGame = new Component(this.bestGameBlock.root, 'div', ['best-game']);
-    this.bestGameTitle = new Component(this.bestGame.root, 'p', [], 'Лучшая игра');
-    this.bestGameCount = new Component(this.bestGame.root, 'p', [], '');
-
-    this.selectGame = new Component(this.root, 'div', ['select-game']);
-    this.selectBtnAudioCall = new Component(this.selectGame.root, 'button', ['select-audio-call'], 'Аудиовызов');
-    this.selectBtnSprint = new Component(this.selectGame.root, 'button', ['select-sprint'], 'Спринт');
+    
  }
 }
--- src/common/header/header.ts
+++ src/common/header/header.ts
@@ -17,16 +17,16 @@ export class Header extends Component {
   pathTitle: Component;
 
   menuBox: MenuBox;
-  
+
   constructor(parent: HTMLElement, private readonly services: TServices) {
     super(parent, "header");
     this.headerWrapper = new Component(this.root, "div", ["header-wrapper"]);
-    this.pathTitle = new Component(this.headerWrapper.root, 'a', [], null, 'href', '#')
+    this.pathTitle = new Component(this.headerWrapper.root, 'a', ['logo'], null, 'href', '#')
     this.title = new Component(this.pathTitle.root, 'img', [], null,'src', 'assets/logo.png');
 
    this.menuBox = new MenuBox(this.headerWrapper.root)
   
-    this.navigationWrapper = new Component(this.headerWrapper.root, 'div', ['navigation-wrapper']);
+    this.navigationWrapper = new Component(this.menuBox.root, 'div', ['navigation-wrapper']);
     
         
     const nav = [
